c29a17363ebe4bb70cc221c48426c403c266ce42
overskilled237 <ouateedemloic@gmail.com>
2023-04-08 22:32:04 +0100

    Created a rotate function for the tetrominos

--- board.js
+++ board.js
@@ -8,16 +8,38 @@ class Board {
     }
 
     
-    valid(p) {
-        return p.shape.every((row, dy) => {
+    valid(tetromino) {
+        return tetromino.shape.every((row, dy) => {
           return row.every((value, dx) => {
-            let x = p.x + dx;
-            let y = p.y + dy;
+            let x = tetromino.x + dx;
+            let y = tetromino.y + dy;
             return value === 0 || (this.isInsideWalls(x, y) && this.notOccupied(x, y));
           });
         });
-      }
+    }
 
+    //function to rotate the tetrominos
+    rotate (tetrominos, direction) {
+
+        let tetro =JSON.parse(JSON.stringify(tetrominos));
+        if(!tetrominos.hardDropped) {
+            //we transpose the metrix
+            for (let y = 0; y < tetro.shape.length; ++y) {
+                for (let x = 0; x < y; ++x) {
+                    [tetro.shape[x][y], tetro.shape[y][x]] = [tetro.shape[y][x], tetro.shape[x][y]];
+                }
+            }
+            //reverse the columns
+            if (direction === ROTATION.RIGHT) {
+                tetro.shape.forEach((row) => row.reverse());
+            } else if (direction === ROTATION.LEFT) {
+                tetro.shape.reverse();
+            }
+        }
+        return tetro;
+    } 
+
+    //function to prevent the block from moving out of the board
     isInsideWalls(x, y) {
         return x >= 0 && x < COLS && y <= ROWS;
     }
