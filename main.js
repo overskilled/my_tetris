9b6258c1da0901769fb8cd69a6dcb6abc0dd1297
overskilled237 <ouateedemloic@gmail.com>
2023-04-19 09:57:08 +0100

    Add some animation so as to make the game lively ;)

--- main.js
+++ main.js
@@ -47,10 +47,33 @@ document.addEventListener ('keydown', event => {
     }
 });
 
+const time = {start : 0, passed : 0, level : 1000}
+
+//function to get the tetrominos moving constantly (Animation)
+function animation(now = 0) {
+    //unpdate passed time 
+    time.passed = now + time.start;
+
+    //chaeck if the time for the current level has passed
+    if (time.passed > time.level) {
+        //restart counting for tnext level
+        time.start = now;
+        
+        board.drop();
+    }
+
+    //clear the board before drawing the new state
+    cxt.clearRect(0, 0, canvas.width, canvas.height);
+
+    board.draw();
+    requestId = requestAnimationFrame(animation);
+
+}
+
 function play() {
     board.reset();
     piece = new Piece(cxt);
-    piece.draw();
+    animation()
 
     board.piece = piece;
     console.table(board.grid);
